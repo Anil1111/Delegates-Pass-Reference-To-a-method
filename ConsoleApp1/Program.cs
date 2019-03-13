93f55eab44de45ed19e69033c479442a7deb5e47
anilsri.04@Gmail.com <anilsri.04@Gmail.com>
2019-03-13 21:44:27 +0530

    Delegates

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,14 +2,79 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApp1
+
+
+public class MediaStorage
+
 {
-    class Program
+    public delegate int PlayMedia();
+
+    public void ReportResult(PlayMedia playerDelegate)
     {
-        static void Main(string[] args)
+        if (playerDelegate() == 0)
         {
+            Console.WriteLine("Media played successfully.");
         }
+        else
+        {
+            Console.WriteLine("Media did not play successfully.");
+        }
+    }
+}
+
+public class AudioPlayer
+{
+    private int audioPlayerStatus;
+
+    public int PlayAudioFile()
+    {
+        Console.WriteLine("Simulating playing an audio file here.");
+        audioPlayerStatus = 0;
+        return audioPlayerStatus;
+    }
+}
+
+public class VideoPlayer
+{
+    private int videoPlayerStatus;
+
+    public int PlayVideoFile()
+    {
+        Console.WriteLine("Simulating a failed video file here.");
+        videoPlayerStatus = -1;
+        return videoPlayerStatus;
+    }
+}
+
+public class Tester
+{
+    public void Run()
+    {
+        MediaStorage myMediaStorage = new MediaStorage();
+
+        // instantiate the two media players
+        AudioPlayer myAudioPlayer = new AudioPlayer();
+        VideoPlayer myVideoPlayer = new VideoPlayer();
+
+        // instantiate the delegates
+        MediaStorage.PlayMedia audioPlayerDelegate = new
+               MediaStorage.PlayMedia(myAudioPlayer.PlayAudioFile);
+        MediaStorage.PlayMedia videoPlayerDelegate = new
+               MediaStorage.PlayMedia(myVideoPlayer.PlayVideoFile);
+
+        // call the delegates
+        myMediaStorage.ReportResult(audioPlayerDelegate);
+        myMediaStorage.ReportResult(videoPlayerDelegate);
+
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Tester t = new Tester();
+        t.Run();
     }
 }
